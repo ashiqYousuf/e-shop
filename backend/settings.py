cceed86d2c90af61a06b5708f51b9ce87a3aeca2
Ashiq Hussain Kumar <91784076+ashiqYousuf@users.noreply.github.com>
2023-03-14 01:19:10 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'sc%oco$+(3$z$at=z4j)#l-+ym)+_b11389mdt2^12m$bf63%@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'e-shop-jl5z.onrender.com']
 
 
 # Application definition
